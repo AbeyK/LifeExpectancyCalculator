d3b54b52cdb13d927bf7d38c96c6674b4759700d
Sean McHugh <smchugh@PIE_MAIN.pietech.com>
2017-06-28 13:07:56 -0400

    Button stuff and http stuff

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -72,12 +72,13 @@ define('main',['exports', './environment'], function (exports, _environment) {
     });
   }
 });
-define('personalinfo',["exports"], function (exports) {
-    "use strict";
+define('personalinfo',['exports', 'aurelia-fetch-client', 'aurelia-framework'], function (exports, _aureliaFetchClient, _aureliaFramework) {
+    'use strict';
 
     Object.defineProperty(exports, "__esModule", {
         value: true
     });
+    exports.PersonalInfo = undefined;
 
     function _classCallCheck(instance, Constructor) {
         if (!(instance instanceof Constructor)) {
@@ -85,11 +86,41 @@ define('personalinfo',["exports"], function (exports) {
         }
     }
 
-    var PersonalInfo = exports.PersonalInfo = function PersonalInfo() {
-        _classCallCheck(this, PersonalInfo);
+    var _dec, _class;
 
-        this.blah = "";
-    };
+    var PersonalInfo = exports.PersonalInfo = (_dec = (0, _aureliaFramework.inject)(_aureliaFetchClient.HttpClient), _dec(_class = function () {
+        function PersonalInfo(http) {
+            _classCallCheck(this, PersonalInfo);
+
+            this.blah = "";
+            this.httpClient = http;
+
+            this.handleBodyClick = function (e) {
+                console.log("ahh" + e.target);
+            };
+        }
+
+        PersonalInfo.prototype.stuff = function stuff() {
+
+            this.httpClient.fetch('https://maps.googleapis.com/maps/api/geocode/json?address=23139&key=AIzaSyBM9-m7L5132H_bDe3JUn9tlwblTARBRbQ').then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+            });
+        };
+
+        PersonalInfo.prototype.attached = function attached() {
+            document.getElementById("fname").onblur = function () {
+                console.log("hello world");
+            };
+        };
+
+        PersonalInfo.prototype.detached = function detached() {
+            document.removeEventListener('click', this.handleBodyClick);
+        };
+
+        return PersonalInfo;
+    }()) || _class);
 });
 define('results',["exports"], function (exports) {
     "use strict";
@@ -118,6 +149,6 @@ define('resources/index',["exports"], function (exports) {
   function configure(config) {}
 });
 define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>Weeeeee</h1><router-view></router-view></template>"; });
-define('text!personalinfo.html', ['module'], function(module) { module.exports = "<template><h1>This is Personal Infooooooo</h1><input type=\"text\" value.bind=\"this.blah\"><h2>${this.blah}</h2></template>"; });
+define('text!personalinfo.html', ['module'], function(module) { module.exports = "<template><h1>This is Personal Infooooooo</h1><input type=\"text\" value.bind=\"this.blah\"><h2>${this.blah}</h2><form submit.trigger=\"stuff()\"><input type=\"text\" value.bind=\"this.blah\" id=\"fname\"><script>function myFunction(){alert(\"Input field lost focus.\")}document.getElementById(\"fname\").onblur=function(){myFunction()}</script><button type=\"submit\">Add Todo</button></form></template>"; });
 define('text!results.html', ['module'], function(module) { module.exports = "<template><h1>This is resultssssssssssss</h1></template>"; });
 //# sourceMappingURL=app-bundle.js.map
--- src/personalinfo.js
+++ src/personalinfo.js
@@ -1,5 +1,50 @@
+import {HttpClient} from 'aurelia-fetch-client';
+import {inject} from 'aurelia-framework';
+
+@inject(HttpClient)
 export class PersonalInfo {
-    constructor() {
+    constructor(http) {
         this.blah = "";
+        this.httpClient = http;
+
+        this.handleBodyClick = e => {
+            console.log("ahh" + e.target);
+        };
     }
+
+    stuff() {
+        // console.log("ahhhhhhhhhhhhh");
+
+    //     this.httpClient.fetch('https://maps.googleapis.com/maps/api/geocode/json?address=23314&key=AIzaSyBM9-m7L5132H_bDe3JUn9tlwblTARBRbQ')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //      console.log(data);
+    //   });
+
+    //     this.httpClient.fetch('https://maps.googleapis.com/maps/api/geocode/json?address=23314&key=AIzaSyBM9-m7L5132H_bDe3JUn9tlwblTARBRbQ')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //      console.log(data.results[0]);
+    //   });
+
+    //   this.httpClient.fetch('https://maps.googleapis.com/maps/api/geocode/json?address=23139&key=AIzaSyBM9-m7L5132H_bDe3JUn9tlwblTARBRbQ')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //      console.log(data.results[0].address_components[3].short_name);
+    //   });
+
+      this.httpClient.fetch('https://maps.googleapis.com/maps/api/geocode/json?address=23139&key=AIzaSyBM9-m7L5132H_bDe3JUn9tlwblTARBRbQ')
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+      });
+    }
+
+    attached() {
+        document.getElementById("fname").onblur = function() {console.log("hello world")};
+    }
+
+     detached() {
+        document.removeEventListener('click', this.handleBodyClick);
+    }   
 }
